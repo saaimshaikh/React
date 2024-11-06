af95cda5c825eef468221b894c3a087c87321148
saaimshaikh <saaimshaikh4321@gmail.com>
2024-11-06 19:11:51 +0530

    saaim

---
+++ my-react-app/src/Componenets/Home.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+   <div className='head'>
+    <br/>
+   <div id='logo'>
+  <img src='Logo.svg'></img>
+   </div>
+   <div id='navbar'>
+<img src='navv.svg'></img>
+
+   </div>
+   <div id='btn'>
+<button><a href='#'><img src='btnw.svg'></img></a></button>
+   </div>
+   <div id='profile'>
+<img src='dt.svg'></img>
+   </div>
+   </div>
+<div className='hero'>
+<br/><br/><br/><br/>
+   <div id='hd1'>
+
+<img src='sd.svg'></img>
+   </div>
+   <div id='sub'></div>
+   <div id='btn2'>
+   <button><a href='#'><img src='Btn.svg'></img></a></button>
+   </div>
+   <div id='btn3'></div>
+   <div id='img'>
+    <img src='img1.svg'></img>
+   </div>
+      </div>
+      <div className='starline'>
+      <img src='img2.svg'></img>
+      </div>
+      <div className='fourths'>
+        <br/>
+      <div id='trade'></div>
+      <div id='wallet'><img src='c.svg'></img></div><br/>
+      <div className='innerwallet'><br/>
+        <div id='b1'><img src='w1.svg'></img></div>
+        <div id='b2'><img src='w2.svg'></img></div>
+        <div id='b3'><img src='w3.svg'></img></div>
+        <div id='b4'><img src='w4.svg'></img></div>
+       
+      </div><br/><br/><br/>
+      <div className='explore'><br/>
+      <div id='how'></div>
+      <div id='setup'>
+        <img src='img3.svg'></img>
+      </div>
+      <div id='setup2'><img src='huu.svg'></img></div>
+      <div id='setup3'><img src='we.svg'></img></div>
+      <div id='setup4'><img src='dsd.svg'></img></div>
+      </div>
+      <div className='explore2'>
+        <img src='www.svg'></img>
+         </div>
+         <br/> <br/> <br/>
+         <div className='box'>
+          <br/>
+          <img src='wq.svg'></img>
+         </div>
+         <br/>
+         <div className='box1'>
+          <br/>
+          <img src='wwww.svg'></img>
+         </div>
+         <br/>
+         <div className='box2'>
+          <br/>
+          <img src='wwwww.svg'></img>
+         </div>
+         <br/><br/><br/><br/>
+         <div className='ready'>
+         <img src='wet.svg'></img>
+         </div><br/>
+         <div className='footer'>
+            <img src='cpr.svg'></img>
+         </div>
+     </div>
+    
+   </>
+  )
+}
+
+export default Home
